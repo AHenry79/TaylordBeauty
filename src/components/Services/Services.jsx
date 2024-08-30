4fd3a11a069575436f29db038610be25b5be9820
AHenry79 <austinhenry7799@gmail.com>
2024-08-30 18:16:48 -0400

    Finish example services page

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -7,6 +7,10 @@ function Services() {
     section.scrollIntoView({ behavior: "smooth" });
   };
 
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
     <div className="services-page">
       <div className="service-list">
@@ -40,8 +44,8 @@ function Services() {
         <h1 className="service-title">MAKEUP</h1>
         <div className="service-info-wrapper">
           <div className="service-info">
-            <h3>Glam service $150</h3>
-            <p className="service-p">
+            <h3 className="service-h3-makeup">Glam service $150</h3>
+            <p className="service-p-makeup">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
               sequi unde aliquid quaerat pariatur veniam vero qui cupiditate
               quidem sint, neque sapiente sed deserunt quibusdam eum eaque quas
@@ -51,11 +55,11 @@ function Services() {
               voluptates voluptatibus error est quo tempore illo.
             </p>
             <br />
-            <p className="service-p">
+            <p className="service-p-makeup">
               Please{" "}
-              <a href="/contact" className="link">
+              <Link to={"/contact"} className="link" onClick={scrollToTop}>
                 contact
-              </a>{" "}
+              </Link>{" "}
               Taylor if your look leans more extreme or extra glam to manage
               time expectations
             </p>
@@ -67,7 +71,7 @@ function Services() {
               pre-planned wedding bookings)
             </p>
             <p>Located at Tricho Studio Salon, Ann Arbor, MI</p>
-            <Link to={"/contact"}>
+            <Link to={"/contact"} onClick={scrollToTop}>
               <button className="book-button">BOOK IN WITH TAYLOR</button>
             </Link>
           </div>
@@ -75,55 +79,99 @@ function Services() {
       </div>
       <div id="facial-waxing">
         <h1 className="service-title">FACIAL WAXING</h1>
-        <h3>Waxing Service $30</h3>
-        <p className="service-p">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro sequi
-          unde aliquid quaerat pariatur veniam vero qui cupiditate quidem sint,
-          neque sapiente sed deserunt quibusdam eum eaque quas eos commodi.{" "}
-          <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-          sequi amet magni, excepturi repellat ratione asperiores ipsum incidunt
-          fugiat nesciunt dolor numquam, earum voluptates voluptatibus error est
-          quo tempore illo.
-        </p>
+        <div className="service-container">
+          <div className="service-image-container">
+            <img
+              src="./src/assets/1722382125401.jpg"
+              alt="facial wax example"
+              className="facial-wax-image"
+            />
+          </div>
+          <div className="service-info-container">
+            <h3 className="service-h3-face-wax">Waxing Service $30</h3>
+            <p className="service-p">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+              sequi unde aliquid quaerat pariatur veniam vero qui cupiditate
+              quidem sint, neque sapiente sed deserunt quibusdam eum eaque quas
+              eos commodi. <br /> Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Sit sequi amet magni, excepturi repellat ratione
+              asperiores ipsum incidunt fugiat nesciunt dolor numquam, earum
+              voluptates voluptatibus error est quo tempore illo.
+            </p>
+          </div>
+        </div>
       </div>
       <div id="body-waxing">
         <h1 className="service-title">BODY WAXING</h1>
-        <h3>Waxing Service $100</h3>
-        <p className="service-p">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro sequi
-          unde aliquid quaerat pariatur veniam vero qui cupiditate quidem sint,
-          neque sapiente sed deserunt quibusdam eum eaque quas eos commodi.{" "}
-          <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-          sequi amet magni, excepturi repellat ratione asperiores ipsum incidunt
-          fugiat nesciunt dolor numquam, earum voluptates voluptatibus error est
-          quo tempore illo.
-        </p>
+        <div className="service-container">
+          <div className="service-info-container right">
+            <h3 className="service-h3 rightside">Waxing Service $100</h3>
+            <p className="service-p rightside">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+              sequi unde aliquid quaerat pariatur veniam vero qui cupiditate
+              quidem sint, neque sapiente sed deserunt quibusdam eum eaque quas
+              eos commodi. <br /> Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Sit sequi amet magni, excepturi repellat ratione
+              asperiores ipsum incidunt fugiat nesciunt dolor numquam, earum
+              voluptates voluptatibus error est quo tempore illo.
+            </p>
+          </div>
+          <div className="service-image-container">
+            <img
+              src="./src/assets/1722382125401.jpg"
+              alt="facial wax example"
+              className="facial-wax-image"
+            />
+          </div>
+        </div>
       </div>
       <div id="brow-tinting">
         <h1 className="service-title">BROW TINTING</h1>
-        <h3>Tinting Service $80</h3>
-        <p className="service-p">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro sequi
-          unde aliquid quaerat pariatur veniam vero qui cupiditate quidem sint,
-          neque sapiente sed deserunt quibusdam eum eaque quas eos commodi.{" "}
-          <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-          sequi amet magni, excepturi repellat ratione asperiores ipsum incidunt
-          fugiat nesciunt dolor numquam, earum voluptates voluptatibus error est
-          quo tempore illo.
-        </p>
+        <div className="service-container">
+          <div className="service-image-container">
+            <img
+              src="./src/assets/1722382125401.jpg"
+              alt="facial wax example"
+              className="facial-wax-image"
+            />
+          </div>
+          <div className="service-info-container">
+            <h3 className="service-h3-face-wax">Tinting Service $80</h3>
+            <p className="service-p">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+              sequi unde aliquid quaerat pariatur veniam vero qui cupiditate
+              quidem sint, neque sapiente sed deserunt quibusdam eum eaque quas
+              eos commodi. <br /> Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Sit sequi amet magni, excepturi repellat ratione
+              asperiores ipsum incidunt fugiat nesciunt dolor numquam, earum
+              voluptates voluptatibus error est quo tempore illo.
+            </p>
+          </div>
+        </div>
       </div>
       <div id="facials">
         <h1 className="service-title">FACIALS</h1>
-        <h3>FACIAL Service $50</h3>
-        <p className="service-p">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro sequi
-          unde aliquid quaerat pariatur veniam vero qui cupiditate quidem sint,
-          neque sapiente sed deserunt quibusdam eum eaque quas eos commodi.{" "}
-          <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-          sequi amet magni, excepturi repellat ratione asperiores ipsum incidunt
-          fugiat nesciunt dolor numquam, earum voluptates voluptatibus error est
-          quo tempore illo.
-        </p>
+        <div className="service-container">
+          <div className="service-info-container right">
+            <h3 className="service-h3 rightside">Facial Service $50</h3>
+            <p className="service-p rightside">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+              sequi unde aliquid quaerat pariatur veniam vero qui cupiditate
+              quidem sint, neque sapiente sed deserunt quibusdam eum eaque quas
+              eos commodi. <br /> Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Sit sequi amet magni, excepturi repellat ratione
+              asperiores ipsum incidunt fugiat nesciunt dolor numquam, earum
+              voluptates voluptatibus error est quo tempore illo.
+            </p>
+          </div>
+          <div className="service-image-container">
+            <img
+              src="./src/assets/1722382125401.jpg"
+              alt="facial wax example"
+              className="facial-wax-image"
+            />
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
